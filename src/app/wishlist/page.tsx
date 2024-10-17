0a85b6f86f75fc056fb27ce693f74ac16e064c34
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2024-10-17 15:07:51 +0500

    completed

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import React, { useState } from "react";
+import React from "react";
 import { useWish } from "../../../context/wishlistContext";
 
 type Product = {
