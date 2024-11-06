065d9086cb33348f646ef3a1d1cfc7bc6ace7929
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2024-11-06 14:33:28 +0500

    complete

--- src/app/Components/NavBar.tsx
+++ src/app/Components/NavBar.tsx
@@ -76,26 +76,6 @@ function NavBar() {
           isHamburger ? "top-[330px]" : ""
         }`}
       >
-        {/* SIGN IN ICON         */}
-        <Link
-          href=""
-          className={`relative ${isHamburger ? "top-[330px]" : ""}`}
-        >
-          <svg
-            className="h-6 w-8 hover:text-[#898989]"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            {" "}
-            <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />{" "}
-            <circle cx="12" cy="7" r="4" />
-          </svg>
-        </Link>
-
         {/* HEART ICON */}
         <Link
           href="/wishlist"
